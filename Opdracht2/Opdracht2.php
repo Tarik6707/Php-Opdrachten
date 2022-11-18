2acf6d656c5cf7cceeecae43cfc0c353822ede95
Tarik6707 <9019582@student.zadkine.nl>
2022-11-18 10:32:34 +0100

    first commit

---
+++ Opdracht2/Opdracht2.php
@@ -0,0 +1,25 @@
+<?php
+
+//  Een voorbeeld van een echo en een print
+echo "<h1>Hallo wereld,</h1>";
+echo "Ik zit op het ";
+
+// Voorbeeld van een variabele in PHP.
+$schoolnaam = "Techniek College Rotterdam !";
+
+// Met echo kun je iets op het scherm tonen.
+echo "$schoolnaam";
+
+// This is a single-line comment
+
+# This is also a single-line comment
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+?>
+
---
+++ Opdracht3/opdracht3.php
@@ -0,0 +1,18 @@
+<?php
+
+$uur = date("H");
+
+//$uur = 15;
+//echo $uur;
+
+if($uur > 6 & $uur <12)
+{
+    echo "Het is ochtend";
+}
+/*
+elseif($uur > 11 & $uur <18)
+{
+    echo "Het is middag";
+}
+*/
+?>
